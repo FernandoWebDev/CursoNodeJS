abcc219959f48e490931031e4bdb19992f989cc7
fernando-pereira-poptrade <fernando.pereira@poptrade.com.br>
2018-03-29 09:36:01 -0300

    Alterei a string de conexão

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,14 +2,13 @@ var mysql = require('mysql');
 
 var connMySQL = function(){
 	return mysql.createConnection({
-		host: 'sql9.freemysqlhosting.net',
-		user: 'sql9225592',
-		password: 'NWcAyqaDaR',
-		database: 'sql9225592'
-	});	
+		host: 'a',
+		user: 'a',
+		password: 'a',
+		database: 'a'
+	});
 }
 
 module.exports = function() {
 	return connMySQL;
 };
-
